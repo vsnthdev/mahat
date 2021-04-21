b5386b5785d88205a70de348a3a646316f9220f1
Vasanth Developer <vasanth@vasanthdeveloper.com>
2021-04-22 00:44:25 +0530

    ✨ created index route

---
+++ api/index.ts
@@ -0,0 +1,19 @@
+/*
+ *  Respond with critical profile information.
+ *  Created On 21 April 2021
+ */
+
+import { VercelRequest, VercelResponse } from '@vercel/node'
+
+const data = {
+    name: 'Vasanth Srivatsa',
+    username: ['vasanthdeveloper', 'Vasanth Developer'],
+    bio: 'Love 💻 computers and makes educational 📹 videos on YouTube.',
+}
+
+export default (req: VercelRequest, res: VercelResponse): VercelResponse => {
+    if (req.url != '/') return res.redirect(308, '/')
+
+    res.setHeader('cache-control', 'public, max-age=3600')
+    return res.status(200).json(data)
+}

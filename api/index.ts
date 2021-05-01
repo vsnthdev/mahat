d88ab3625b51425e3555e73199b1efe41305fa14
Vasanth Developer <vasanth@vasanthdeveloper.com>
2021-05-02 02:12:33 +0530

    📇 GET / now pulls avatar from Twitter

--- api/index.ts
+++ api/index.ts
@@ -5,24 +5,46 @@
  */
 
 import { VercelRequest, VercelResponse } from '@vercel/node'
+import { TwitterClient } from 'twitter-api-client'
 
-const data = {
+export const data = {
     name: 'Vasanth Srivatsa',
     username: 'Vasanth Developer',
     email: 'vasanth@vasanthdeveloper.com',
     bio: 'Love 💻 computers and makes educational 📹 videos on YouTube.',
+    avatar: null,
     social: {
-        YouTube: 'https://youtube.com/vasanthdeveloper',
-        Twitter: 'https://twitter.com/vasanthdevelop',
-        GitHub: 'https://github.com/vasanthdeveloper',
-        Discord: 'https://vas.cx/discord',
-        Telegram: 'https://t.me/vasanthdeveloper',
+        youtube: 'vasanthdeveloper',
+        twitter: 'vasanthdevelop',
+        github: 'vasanthdeveloper',
+        telegram: 'vasanthdeveloper',
     },
 }
 
-export default (req: VercelRequest, res: VercelResponse): VercelResponse => {
+export default async (
+    req: VercelRequest,
+    res: VercelResponse,
+): Promise<VercelResponse> => {
     if (req.url != '/') return res.redirect(308, '/')
 
     res.setHeader('cache-control', 'public, max-age=86400')
+
+    // create an authenticated Twitter client
+    const twitter = new TwitterClient({
+        apiKey: process.env.TWITTER_API_KEY,
+        apiSecret: process.env.TWITTER_API_SECRET,
+        accessToken: process.env.TWITTER_ACCESS_KEY,
+        accessTokenSecret: process.env.TWITTER_ACCESS_SECRET,
+    })
+
+    const profile = await twitter.accountsAndUsers.usersLookup({
+        screen_name: data.social.twitter,
+    })
+
+    data.avatar = profile[0].profile_image_url_https.replace(
+        '_normal',
+        '_400x400',
+    )
+
     return res.status(200).json(data)
 }

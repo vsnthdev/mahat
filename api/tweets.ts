3edd5adf29361d4979ba82aac0e7d7074b5fdb85
Vasanth Developer <vsnthdev@gmail.com>
2022-04-24 18:20:37 +0530

    ✨ implemented caching for GET /tweets

--- api/tweets.ts
+++ api/tweets.ts
@@ -3,8 +3,9 @@
  *  Created On 02 May 2021
  */
 
-import { VercelRequest, VercelResponse } from '@vercel/node'
 import axios from 'axios'
+import { VercelRequest, VercelResponse } from '@vercel/node'
+import { getCache, setCache } from '../lib/prisma'
 
 import { cors } from './index'
 
@@ -19,6 +20,16 @@ export default async (
 
     // work on headers
     await cors(req, res)
+
+    // set the cache header to not cached by default
+    res.setHeader('m-cached', 'false')
+
+    // get from the cache
+    const cache = await getCache('tweets', 10)
+    if (cache) {
+        res.setHeader('m-cached', 'true')
+        return res.status(200).json(cache)
+    }
 
     // fetch the Twitter tweets from
     // my Discord channel's tweets channel
@@ -40,6 +51,9 @@ export default async (
             url: content[2],
         }
     }
+
+    // cache the response for next time
+    await setCache('tweets', tweets)
 
     return res.status(200).json(tweets)
 }
